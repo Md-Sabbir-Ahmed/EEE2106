a2870ec9da9e04fa388925dcea3da738970cdd4b
MD. SABBIR AHMED <sabbir206314@gmail.com>
2021-12-22 19:24:38 +0600

    1.1 to 1.6

---
+++ LAB 01/Chapter 01/Example02/Example02.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    cout<<"Hel"<<"lo wo"<<"rld!"<<endl;
+    cout<<"I'm"<<" a C+"<<"+ program. "<<endl;
+    return 0 ;
+}
---
+++ LAB 01/Chapter 01/Example03/Example03.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    cout<<"Hello world!"<<endl;
+    cout<<"I'm a C++ program. "<<endl;
+    cout<<"I'll show you todays' date. "<<endl;
+    cout<<"Today is January "<<15<<" "<<2021<<"."<<endl;
+    return 0 ;
+}
+
