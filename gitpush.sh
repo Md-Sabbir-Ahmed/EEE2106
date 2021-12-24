41d3767495261c50b3dd1da398f4d2be0ca9b415
MD. SABBIR AHMED <sabbir206314@gmail.com>
2021-12-24 16:36:32 +0600

    gitpush added

---
+++ gitpush.sh
@@ -0,0 +1,14 @@
+git add .
+
+echo 'Hello Sabbir'
+echo 'Welcome to EEE2110'
+echo 'Enter your commit message:'
+read commitMessage
+
+git commit -m "$commitMessage"
+
+git push 
+
+echo 'Your EEE2110 Successfully Updated'
+
+read
