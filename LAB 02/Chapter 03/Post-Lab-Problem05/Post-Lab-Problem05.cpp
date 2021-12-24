33aa9d04296e8baaf839283ac6cee6d22f25eb6e
MD. SABBIR AHMED <sabbir206314@gmail.com>
2021-12-24 23:35:33 +0600

    PLP 3.5

---
+++ LAB 02/Chapter 03/Post-Lab-Problem05/Post-Lab-Problem05.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char c;
+    cout<<"Enter your Letter: ";
+    cin>>c;
+    switch(c)
+    {
+    case 'a':
+        cout<<"Vowel";
+        break;
+    case 'e':
+        cout<<"Vowel";
+        break;
+    case 'i':
+        cout<<"Vowel";
+        break;
+    case 'o':
+        cout<<"Vowel";
+        break;
+    case 'u':
+        cout<<"Vowel";
+        break;
+    default:
+        cout<<"Consonant";
+        break;
+    }
+    return 0 ;
+}
